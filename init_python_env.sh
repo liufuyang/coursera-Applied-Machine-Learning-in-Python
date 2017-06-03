5228409d68339f4083a740759f1462c38cc6e5b5
Fuyang Liu <fli@tradeshift.com>
2017-06-03 12:03:51 +0200

    Add init env script

---
+++ init_python_env.sh
@@ -0,0 +1,13 @@
+
+PYTHON_ENV_NAME=venv
+
+pip install virtualenv
+
+virtualenv -p python3 $PYTHON_ENV_NAME
+
+echo "source $(pwd)/$PYTHON_ENV_NAME/bin/activate" > .env
+
+source $(pwd)/$PYTHON_ENV_NAME/bin/activate # activate the local python environment
+
+pip install jupyter
+pip install scikit-learn
